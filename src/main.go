4b2769d39ce085e2b677926b96502f96eac193e7
Maria Paktiti <maria@powertools.dev>
2023-11-14 20:04:13 -0600

    feat: update regular expression for article name to include special chars

--- src/main.go
+++ src/main.go
@@ -12,9 +12,9 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/articles/top/weekly/{year:[0-9]+}/{week:[0-9]+}", handler.TopArticlesWeeklyHandler)
 	r.HandleFunc("/articles/top/monthly/{year:[0-9]+}/{month:[0-9]+}", handler.TopArticlesMonthlyHandler)
-	r.HandleFunc("/article/{article:[\\w%]+}/weekly/{year:[0-9]+}/{week:[0-9]+}", handler.ViewsPerArticleWeeklyHandler)
-	r.HandleFunc("/article/{article}/monthly/{year}/{month}", handler.ViewsPerArticleMonthlyHandler)
-	r.HandleFunc("/article/{article}/top/monthly/{year}/{month}", handler.TopViewsPerArticleMonthlyHandler)
+	r.HandleFunc("/article/{article:[0-9a-zA-Z_%,-.~()'!:@;*]+}/weekly/{year:[0-9]+}/{week:[0-9]+}", handler.ViewsPerArticleWeeklyHandler)
+	r.HandleFunc("/article/{article:[0-9a-zA-Z_%,-.~()'!:@;*]+}/monthly/{year}/{month}", handler.ViewsPerArticleMonthlyHandler)
+	r.HandleFunc("/article/{article:[0-9a-zA-Z_%,-.~()'!:@;*]+}/top/monthly/{year}/{month}", handler.TopViewsPerArticleMonthlyHandler)
 	http.Handle("/", r)
 
 	log.Println("Listening on localhost:8080")
